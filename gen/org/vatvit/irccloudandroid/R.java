28c841056785f86487f2ceece0d2ab6af20f5ad6
Jaakko Lukkari <jaakko.lukkari@gmail.com>
2011-06-28 21:40:05 +0300

    Added login test

--- gen/org/vatvit/irccloudandroid/R.java
+++ gen/org/vatvit/irccloudandroid/R.java
@@ -13,11 +13,27 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int emailField=0x7f050003;
+        public static final int emailLabel=0x7f050001;
+        public static final int loginButton=0x7f050008;
+        public static final int passwordField=0x7f050006;
+        public static final int tableLayout1=0x7f050000;
+        public static final int tableRow2=0x7f050004;
+        public static final int tableRow3=0x7f050007;
+        public static final int tableRow4=0x7f050009;
+        public static final int tableRow5=0x7f05000a;
+        public static final int textView1=0x7f050002;
+        public static final int textView2=0x7f050005;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
+        public static final int Email=0x7f040002;
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int login=0x7f040004;
+        public static final int password=0x7f040003;
     }
 }
--- src/org/vatvit/irccloudandroid/MainActivity.java
+++ src/org/vatvit/irccloudandroid/MainActivity.java
@@ -1,13 +1,48 @@
 package org.vatvit.irccloudandroid;
 
+import org.json.JSONObject;
+import org.vatvit.irccloud.Connection;
+import org.vatvit.irccloud.events.EventListener;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	private static final String TAG = "IRCCloudMainActivity";
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+        final EditText emailField = (EditText) findViewById(R.id.emailField);
+        final EditText passwordField = (EditText) findViewById(R.id.passwordField);
+        
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Connection ircConn = new Connection(emailField.getText().toString(), passwordField.getText().toString());
+        		
+        		ircConn.addEventListener(new EventListener(){
+        			public void onEvent(JSONObject event) {
+        				Log.v(TAG, "Event: "+event.toString());
+        			}	
+        		});
+        		
+        		if(ircConn.login()) {
+        			Log.v(TAG, "Login successful. Session: "+ircConn.getSession());
+        		} else {
+        			Log.v(TAG, "Login failed.");
+        		}
+  
+            }
+        });
+ 
     }
 }
